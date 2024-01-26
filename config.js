8dee8601252f52ce1bb8d7f8d40cf94d96aa55f8
xaioloungbao <156146467+xaioloungbao@users.noreply.github.com>
2024-01-26 15:23:11 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  challenge: false,
+  challenge: true,
   users: {
     // username: 'password', you can add multiple users.
-    interstellar: 'password',
+    suuuiii: 'iamnotstupid',
   }
 }
 export default config
